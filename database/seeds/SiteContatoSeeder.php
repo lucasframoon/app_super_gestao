9df3f8b8830ed8eee349c4dbbce412c4be4eb308
Lucas Franco <lucasframoon@gmail.com>
2022-06-25 23:22:07 -0300

    feat criando seeders

---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*$contato = new SiteContato();
+        $contato->nome = 'Sistema SG';
+        $contato->telefone = '(11) 98765-4321';
+        $contato->email = 'contato@sg.com.br';
+        $contato->motivo_contato = 1;
+        $contato->mensagem = 'Bem vindo ao sistema Super Gestão';
+        
+        $contato->save();*/
+
+        factory(SiteContato::class, 100)->create();;
+
+    }
+}
