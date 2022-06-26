e2bac50c4795d9522c2126c6e6ef5abefe6e323f
Lucas Franco <lucasframoon@gmail.com>
2022-06-25 23:23:23 -0300

    feat refatorando front e criando tabela de motivos

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
+use App\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        // var_dump($_POST);
-        return view('site/contato');
+    public function contato(Request $request){
+
+        $motivo_contatos =  MotivoContato::all();
+
+        return view('site/contato', ['titulo'=> 'Contato (teste)', 'motivo_contatos'=> $motivo_contatos]);
+    }
+
+    public function salvar(Request $request){
+        // dd($request);
+        //validando dados
+        $request->validate([
+            'nome' => 'required|min:3|max:40'
+            /*'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'*/
+        ]);
+        // SiteContato::create($request->all());
     }
 }
