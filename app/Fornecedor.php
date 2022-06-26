be27c6350b0d61c1c628725dadc3a237d5e73f63
Lucas Franco <lucasframoon@gmail.com>
2022-06-25 23:21:50 -0300

    feat softDeletes Fornecedor

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    
+    use SoftDeletes;
+
     protected $table = 'fornecedores';//Correção no nome padrão da tabela    
     protected $fillable = ['nome', 'site','uf', 'email'];
 }
